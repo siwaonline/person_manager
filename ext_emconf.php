1d241beb3e2be7dea69f78e2c5f837cb4a630426
Philipp Parzer <p.parzer@siwa.at>
2020-02-13 10:58:06 +0100

    Raise Version to TYPO3 9 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,10 +22,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '1',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '2.1.0',
+	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-8.7.99',
+			'typo3' => '9.5.0-9.5.99',
 		),
 		'conflicts' => array(
 		),
